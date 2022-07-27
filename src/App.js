41da77097560ee46f3f88a2fa869e50bcb8cf148
instamenta <98179343+instamenta@users.noreply.github.com>
2022-07-27 13:53:28 +0200

    bug fix router-dom implementation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+//import { useEffect, useState, Suspense } from "react";
+import { Routes, Route } from 'react-router-dom';
 
+import { Navbar } from "./components/navbar/navbar"
+import "./App.css"
 function App() {
-    
+
 
     return (
-        <div>
-            <nav>
-                <ul>
-                <li>saddsadsa</li>
-                <li>saddsadsa</li>
-                <li>saddsadsa</li>
-                <li>saddsadsa</li>
-                <li>saddsadsa</li>
-                    <li>saddsadsa</li>
-                    <li>saddsadsa</li>
-                </ul>
-            </nav>
+        <div id="box">
+            <Navbar />  
+            <main id="main-content">
+                <Routes>
+                    <Route path="/" />
+                    <Route path="/login" />
+                    <Route path="/register" />
+                    <Route path="/create"  />
+                    <Route path="/catalog" />
+                    <Route path="/catalog/:gameId" />
+                </Routes>
+            </main>
         </div>
     );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+export const Navbar = () => {
+    return (
+        <div id="navbar-main-div">
+            <div className="html-container">
+                <div className="html-section">
+                    <div className="html-project">
+                        <div className="navigation">
+                            <nav>
+                                <ul className="nav-type">
+                                    <li>
+                                        <Link to="https://codepen.io/EneergeticTomy/" className="active">
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            to="https://codepen.io/TomikaBoy/full/LqMpaE"
+                                            className="active1"
+                                        >
+                                            About
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            to="https://codepen.io/"
+                                             
+                                            className="active2"
+                                        >
+                                            Contact
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            to="https://w3schools.com/"
+                                             
+                                            className="active3"
+                                        >
+                                            Support
+                                        </Link>
+                                    </li>
+                                    <div className="line" />
+                                    <li>
+                                        {/*<Link>
+                                            <i className="fa fa-search" onclick="show()"></i>
+                                        </Link>*/}
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <noscript>
+                    &lt;div class="first-line"&gt; &lt;div class="linear"&gt; &lt;div
+                    class="line-w5"&gt; &lt;/div&gt; &lt;/div&gt; &lt;/div&gt;
+                </noscript>
+                <template />
+                <noscript>
+                    &lt;div class="cyrcle-center"&gt; &lt;div class="radial-cyrcle"&gt;
+                    &lt;/div&gt; &lt;/div&gt;
+                </noscript>
+            </div>
+            <template />
+            <div className="dropdown" id="anotherFunction">
+                <div className="drop1">HTML</div>
+                <div className="drop2">CSS</div>
+                <div className="drop3">Javascript</div>
+                <div className="drop4">Sass</div>
+            </div>
+        </div>
+    )
+}
