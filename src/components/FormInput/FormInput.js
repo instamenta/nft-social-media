55deb2f6d33645d907f5f7829694e3bf18ce9613
instamenta <98179343+instamenta@users.noreply.github.com>
2022-08-09 18:16:20 +0200

    register form css done and client-side validation

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,13 +1,23 @@
+import { useState } from "react"
 import "./FormInput.css"
 export const FormInput = (props) => {
-    const {label, onChange, id, ...inputProps} = props
+    const [focused, setFocused] = useState(false)
+    const {label, errorMessage, onChange, id, ...inputProps} = props
+
+    const handleFocus = ( e ) => {
+        setFocused(true)
+    }
     return(
         <div className="formInput">
-            <label>{label}</label>
+            <label for={props.name}>{label}</label>
             <input 
             {...inputProps}
             onChange={onChange}
+            onBlur={handleFocus}
+            onFocus={()=>inputProps.name ==="confirmPassword" && setFocused(true)}
+            focused={focused.toString()}
             ></input>
+            <span>{errorMessage}</span>
         </div>
     )
 }
