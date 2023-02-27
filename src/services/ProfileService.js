e49f3841d1ec86f6799df4ae6b04643b5fb7946d
instamenta <98179343+instamenta@users.noreply.github.com>
2023-02-27 04:19:25 +0200

    Update ProfileService.js

--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export async function getUser (id) {
     const { data } = await axios.get(`http://localhost:3031/profile/` + id)
-
     if (data?.message) {
         return data.message
     } else {
@@ -21,3 +20,33 @@ export async function getUserByName (username, password) {
 }
 
 
+export async function editUserPicture (id, username, password) {
+    // not added
+    const { data } = await axios.post(`http://localhost:3031/profile/${id}/edit-user-picture`, {username,password})
+    if (data?.message) {
+        return data.message
+    } else {
+        return data
+    }
+}
+export async function editUserData (id, username, email) {
+    const { data } = await axios.post(`http://localhost:3031/profile/${id}/edit-user-data`, 
+    { username, email })
+    if (data?.message) {
+        return data.message
+    } else {
+        return data
+    }
+}
+export async function editUserBio (id, username, bio ) {
+    const { data } = await axios.post(`http://localhost:3031/profile/${id}/edit-user-bio`, {username, bio})
+    if (data?.message) {
+        return data.message
+    } else {
+        return data
+    }
+}
+// router.route('/:id/edit-user-picture').post(editUserPicture)
+
+// router.route('/:id/edit-user-name').post(editUserName)
+// router.route('/:id/edit-user-bio').post(editBioUser)
