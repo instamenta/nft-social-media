c7e49bf634dffb7fcd667fa9cb75ca23a8f13c49
instamenta <98179343+instamenta@users.noreply.github.com>
2023-02-28 01:32:24 +0200

    Revert "remove"
    This reverts commit fc577eacb8baebce5719e98f6b49804e7880f72a.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -49,7 +49,7 @@ export const Login = () => {
         if (loginData === 'Invalid username or password') {
             setErrors('Invalid username or password')
         } else {
-            Cookies.set('user', loginData.token)
+            Cookies.set('user',loginData.token)
             setAuth(loginData)
             navigate('/')
         }
@@ -58,38 +58,28 @@ export const Login = () => {
         setValues({ ...values, [e.target.name]: e.target.value })
     }
     return (
-        <div className='main'>
-            <div className='login-container'>
-                <div className='login-decoration'>
-
-                    <h1 className='auth-h1'><i class="fa-solid fa-user-lock"></i> Login</h1>
-
-                </div>
-                <form
-                    onSubmit={handleSubmit}
-                    className='form-container'>
-                    <h2 className='try-again-message'>
-                        {errors}
-                    </h2>
-                    {inputs.map((input) => (
-                        <FormInput
-                            key={input.id}
-                            {...input}
-                            value={values[input.name]}
-                            onChange={onChange}
-                        />))}
-
-                    <button className='submit-button'>
-                        Login
-                    </button>
-
-                    <Link to="/users/register" className='auth-redirect'>
-                        Redirect to register.
-                    </Link>
-                </form>
-                <div className='login-decoration-bottom'>
-                </div>
-            </div>
+        <div>
+            <form
+                onSubmit={handleSubmit}
+                className='form-container'>
+                <h1 className='auth-h1'>Login</h1>
+                <h2 className='try-again-message'>
+                    {errors}
+                </h2>
+                {inputs.map((input) => (
+                    <FormInput
+                        key={input.id}
+                        {...input}
+                        value={values[input.name]}
+                        onChange={onChange}
+                    />))}
+                <Link to="/users/register" className='auth-redirect'>
+                    Redirect to register
+                </Link>
+                <button className='submit-button'>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
